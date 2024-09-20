c2abab6093e81207f7b723f51466a18b2567aa09
ella wang <ellawang@ellas-MacBook-Air.local>
2024-09-20 23:26:25 +0800

    compare between customer and standar poses

--- app/(routes)/(menuBar)/a2/page.tsx
+++ app/(routes)/(menuBar)/a2/page.tsx
@@ -4,12 +4,15 @@ import { useEffect, useRef, useState } from 'react'
 //import DummyPage from '../../../_components/DummyPage'
 import BottomMenuComponent from '@components/BottomMenuComponent'
 import * as poseDetection from '@tensorflow-models/pose-detection';
-import * as tf from '@tensorflow/tfjs-core';
+//import * as tf from '@tensorflow/tfjs-core';
 import '@tensorflow/tfjs-backend-webgl';
 
 export default function ContactPage() {
     const videoRef = useRef(null);
     const canvasRef = useRef(null);
+    const imageRef = useRef(null);
+    //const [standarPoses, setStandarPoses] = useState(null);
+    const [adjustStandarKeypoints, setAdjustStandarKeypoints] = useState(null);
     const [isCameraOn, setCamerabtnStatus] = useState(false);
     const [detector, setDetector] = useState(null);
 
@@ -18,6 +21,10 @@ export default function ContactPage() {
         const detectorConfig = {modelType: poseDetection.movenet.modelType.SINGLEPOSE_LIGHTNING};
         const de = await poseDetection.createDetector(model, detectorConfig);
         setDetector(de);
+
+//         const imgElement = imageRef.current;
+//         const result = await de.estimatePoses(imgElement);
+//         setStandarPoses(result);
     }
 
     useEffect(() => {
@@ -56,10 +63,6 @@ export default function ContactPage() {
             //console.log(pose.keypoints);
             pose.keypoints.forEach((keypoint) => {
                 const { x, y, score, name } = keypoint;
-//                 if (name === 'left_wrist') {
-//                     console.log(x, y, score)
-//                     const leftHip = keypoint;
-//                 }
                 if (score > threshold) {
                     ctx.beginPath();
                     ctx.arc(x, y, 5, 0, 2 * Math.PI);
@@ -90,25 +93,107 @@ export default function ContactPage() {
 
     useEffect(() => {
         if (detector) {
+            async function scanStandar () {
+                const imgElement = imageRef.current;
+                console.log("before");
+                const standarPoses = await detector.estimatePoses(imgElement);
+                const shoulderDistance = getShoulderDistance(standarPoses[0].keypoints);
+                const [hipDistance, leftHip, rightHip] = getHipDistance(standarPoses[0].keypoints);
+                const scale = hipDistance; // or shoulderDistance
+                const centerX = (leftHip.x + rightHip.x) / 2;
+                const centerY = (leftHip.y + rightHip.y) / 2;
+                console.log(centerY, centerX);
+                const result = standarPoses[0].keypoints.map(({y, x, score, name}) => {
+                    return {
+                        y: (y - centerY), // scale by hipDistance
+                        x: (x - centerX), // offset by center
+                        score: score,
+                        name: name
+                    };
+                });
+                const aa = poseDetection.calculators.keypointsToNormalizedKeypoints(result, {width: 640, height: 480})
+                setAdjustStandarKeypoints(aa);
+            }
+            scanStandar();
+        }
+    }, [detector]);
+
+    useEffect(() => {
+        if (detector) {
             const interval = setInterval(async () => {
                 const videoElement = videoRef.current;
                 if (videoElement && videoElement.readyState === 4) {
                     const poses = await detector.estimatePoses(videoElement);
-                    //console.log(poses);
                     const canvas = canvasRef.current;
                     const ctx = canvas.getContext('2d');
                     ctx.clearRect(0, 0, canvas.width, canvas.height);
-                    //resetCenter(poses);
+                    compareWithStandar(poses);
                     drawKeypoints(ctx, poses);
                     drawSkeleton(ctx, poses);
                 }
-            }, 100);
-            return () => clearInterval(interval);
+            }, 1000);
+            return () => {
+                clearInterval(interval);
+                console.log("clear");
+            }
         }
-    });
+    }, [detector, adjustStandarKeypoints]);
+
+    function getShoulderDistance(keypoints) {
+        const leftShoulder = keypoints[5].name === 'left_shoulder' ? keypoints[5] : keypoints.find(point => point.name === 'left_shoulder');
+        const rightShoulder = keypoints[6].name === 'right_shoulder' ? keypoints[6] : keypoints.find(point => point.name === 'right_shoulder');
+        const shoulderDistance = keypointsDistance(leftShoulder, rightShoulder);
+        return shoulderDistance;
+    }
+
+    function getHipDistance(keypoints) {
+        const leftHip = keypoints[11].name === 'left_hip' ? keypoints[11] : keypoints.find(point => point.name === 'left_hip');
+        const rightHip = keypoints[12].name === 'right_hip' ? keypoints[12] : keypoints.find(point => point.name === 'right_hip');
+        const hipDistance = keypointsDistance(leftHip, rightHip);
+        return [hipDistance, leftHip, rightHip];
+    }
+
+    function keypointsDistance(point1, point2) {
+        const dx = point1.x - point2.x;
+        const dy = point1.y - point2.y;
+        return Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+    }
+
+    function compareWithStandar(poses) {
+        const shoulderDistance = getShoulderDistance(poses[0].keypoints);
+        const [hipDistance, leftHip, rightHip] = getHipDistance(poses[0].keypoints);
+        const scale = hipDistance; // or shoulderDistance
+        const centerX = (leftHip.x + rightHip.x) / 2;
+        const centerY = (leftHip.y + rightHip.y) / 2;
+        //console.log(centerY, centerX);
+        const adjustedKeypoints = poses[0].keypoints.map(({y, x, score, name}) => {
+            return {
+                y: (y - centerY), // scale by hipDistance
+                x: (x - centerX), // offset by center
+                score: score,
+                name: name
+            };
+        });
+        const aa = poseDetection.calculators.keypointsToNormalizedKeypoints(adjustedKeypoints, {width: 640, height: 480})
+        //console.log(aa[9]);
+
+        if (adjustStandarKeypoints) {
+            //console.log("standar: ", adjustStandarKeypoints[9]);
+            const distances = aa.map((p1, index) => keypointsDistance(p1, adjustStandarKeypoints[index]));
+            const averageDistance = distances.reduce((a, b) => a+b, 0) / distances.length;
+            console.log(averageDistance);
+        }
+    }
+
+    function compareUpperBody(userPoses) {
+    }
+
+    function compareLowerBody(userPoses) {
+    }
 
     return (
         <BottomMenuComponent title="Planner Page">
+            <img className="" ref={imageRef} src="/standar.jpg" width="640" height="480"></img>
             <div className="flex float-left">
                 <canvas ref={canvasRef} width="640" height="480"></canvas>
             </div>
